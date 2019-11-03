772c2b4d1f568f138226a2385203e54012d4d9d9
Ngọc Quân Trần <tranngocquan95vn@gmail.com>
2019-11-03 22:44:46 +0700

    Add DbContext

---
+++ Order/Data/Order.Data/Context/OrderContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Order.Data.Context
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Domain.Models.Order> Orders { get; set; }
+
+        public DbSet<Domain.Models.OrderDetail> OrderDetails { get; set; }
+
+    }
+}
---
+++ Product/Data/Product.Data/Context/ProductContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Product.Data.Context
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Domain.Models.Product> Products { get; set; }
+    }
+}
